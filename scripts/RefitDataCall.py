a5660152a957eddcb6d579c2971185f37d16ddfe
sumit <raje.sumit17@gmail.com>
2025-02-03 20:34:11 +0100

    cleaned dataset used, new plot generated

--- scripts/RefitDataCall.py
+++ scripts/RefitDataCall.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Replace 'refit_dataset.csv' with the path to your dataset file
-df = pd.read_csv("C:\Data\REFITPowerData111215\House1_AddHead.csv")
+df = pd.read_csv("/home/teeker/Data/ets-refit/CLEAN_REFIT_081116/CLEAN_House1.csv")
 print(df.head())
 
 print(df.info())
@@ -13,7 +13,7 @@ print(df.describe())
 # Assuming the dataset has columns 'timestamp' and 'power'
 # df['DateTime'] = pd.to_datetime(df['DateTime'])
 plt.figure(figsize=(10, 6))
-sns.lineplot(x='DateTime', y='Aggregate', data=df)
+sns.lineplot(x='Unix', y='Aggregate', data=df)
 plt.xlabel('Time')
 plt.ylabel('Power Consumption (W)')
 plt.title('Power Consumption Over Time')
